4e455a5078918c931f02c4015d1dd0fdf48c94cb
Andres Colon <andres.colon@scimedsolutions.com>
2018-09-19 11:36:07 -0400

    Configure webpacker and install semantic
    Tempus Task #216882

--- app/client/packs/application.js
+++ app/client/packs/application.js
@@ -7,4 +7,8 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import 'babel-polyfill';
+import 'semantic-ui-css';
+import jQuery from 'jquery';
+window.jQuery = jQuery;
+window.$ = jQuery;
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,24 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
+const webpack = require('webpack');
 
-module.exports = environment
+// resolve-url-loader must be used before sass-loader
+environment.loaders.get('sass').use.splice(-1, 0, {
+  loader: 'resolve-url-loader',
+  options: {
+    attempts: 1
+  }
+});
+
+// Add an ProvidePlugin
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery'
+  })
+);
+
+const config = environment.toWebpackConfig();
+
+module.exports = environment;
