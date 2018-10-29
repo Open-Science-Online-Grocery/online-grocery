6d262f264210ee611d8934f550ab64317c52e6be
Andrea Angott <andrea.angott@scimedsolutions.com>
2018-10-29 11:03:05 -0400

    Styling authentication pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :timeoutable,
-         :recoverable, :rememberable, :validatable, :lockable, :trackable
+         :recoverable, :rememberable, :validatable, :lockable, :trackable,
+         :confirmable
 
   has_many :experiments, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -54,5 +54,4 @@ ActiveRecord::Schema.define(version: 2018_09_24_160613) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
