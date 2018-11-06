35eb55da7ff8a325c855940bef36da118b6aa407
Andrea Angott <andrea.angott@scimedsolutions.com>
2018-11-06 10:59:46 -0500

    Implementing operator and digit buttons
    Task #217932

--- app/client/calculator/src/components/Calculator.jsx
+++ app/client/calculator/src/components/Calculator.jsx
@@ -2,6 +2,13 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import VariableSelectorContainer from '../containers/VariableSelectorContainer';
 import EquationEditorContainer from '../containers/EquationEditorContainer';
+import OperatorButtonRowContainer from '../containers/OperatorButtonRowContainer';
+import DigitButtonRowContainer from '../containers/DigitButtonRowContainer';
+
+const operators1 = ['+', '-', '*', '/', '%', '^', '|', '&'];
+const operators2 = ['<', '>', '<=', '>=', '!=', '=', '(', ')'];
+const operators3 = ['IF', 'AND', 'OR', 'NOT', 'MIN', 'MAX', 'SUM', 'AVG'];
+const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 
 export default class Calculator extends PureComponent {
   render() {
@@ -12,50 +19,10 @@ export default class Calculator extends PureComponent {
         </div>
         <EquationEditorContainer />
         <VariableSelectorContainer />
-
-        <div className="ui buttons operators">
-          <button type="button" className="ui button">+</button>
-          <button type="button" className="ui button">-</button>
-          <button type="button" className="ui button">*</button>
-          <button type="button" className="ui button">/</button>
-          <button type="button" className="ui button">%</button>
-          <button type="button" className="ui button">^</button>
-          <button type="button" className="ui button">|</button>
-          <button type="button" className="ui button">&</button>
-        </div>
-        <div className="ui buttons operators">
-          <button type="button" className="ui button">{'<'}</button>
-          <button type="button" className="ui button">{'>'}</button>
-          <button type="button" className="ui button">{'<='}</button>
-          <button type="button" className="ui button">{'>='}</button>
-          <button type="button" className="ui button">!=</button>
-          <button type="button" className="ui button">=</button>
-          <button type="button" className="ui button">(</button>
-          <button type="button" className="ui button">)</button>
-        </div>
-        <div className="ui buttons operators">
-          <button type="button" className="ui button">IF</button>
-          <button type="button" className="ui button">AND</button>
-          <button type="button" className="ui button">OR</button>
-          <button type="button" className="ui button">NOT</button>
-          <button type="button" className="ui button">MIN</button>
-          <button type="button" className="ui button">MAX</button>
-          <button type="button" className="ui button">SUM</button>
-          <button type="button" className="ui button">AVG</button>
-        </div>
-        <div className="ui buttons digits">
-          <button type="button" className="ui button">0</button>
-          <button type="button" className="ui button">1</button>
-          <button type="button" className="ui button">2</button>
-          <button type="button" className="ui button">3</button>
-          <button type="button" className="ui button">4</button>
-          <button type="button" className="ui button">5</button>
-          <button type="button" className="ui button">6</button>
-          <button type="button" className="ui button">7</button>
-          <button type="button" className="ui button">8</button>
-          <button type="button" className="ui button">9</button>
-          <button type="button" className="ui button">.</button>
-        </div>
+        <OperatorButtonRowContainer values={operators1} className="operators" />
+        <OperatorButtonRowContainer values={operators2} className="operators" />
+        <OperatorButtonRowContainer values={operators3} className="operators" />
+        <DigitButtonRowContainer values={digits} className="digits" />
         <div className="test-button">
           <button type="button" className="ui primary button">
             <i className="calculator icon" />
