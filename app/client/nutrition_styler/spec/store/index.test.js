149507e1832bf5247dd4393ede69077caae58d4d
Andrea Angott <andrea.angott@scimedsolutions.com>
2018-11-15 10:37:37 -0500

    Testing remaining reducers
    Task #216878

--- app/client/nutrition_styler/spec/store/index.test.js
+++ app/client/nutrition_styler/spec/store/index.test.js
@@ -10,4 +10,21 @@ describe('reducers', () => {
       expect(initialState.get('cssSelectors')).toEqual(Immutable.Map());
     });
   });
+
+  describe('activeSelector', () => {
+    const state = '.calories-label';
+
+    it('returns the same state for an unknown action', () => {
+      const action = { type: 'whatever' };
+      expect(fromSubject.activeSelector(state, action)).toEqual(state);
+    });
+
+    it('returns the new selector for SET_ACTIVE_SELECTOR', () => {
+      const action = {
+        type: actions.SET_ACTIVE_SELECTOR,
+        payload: { activeSelector: '.foo' }
+      };
+      expect(fromSubject.activeSelector(state, action)).toEqual('.foo');
+    });
+  });
 });
