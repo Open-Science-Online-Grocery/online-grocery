0f8c8277e6783ee20496c5d4d456e5c0d86c5039
Andrea Angott <andrea.angott@scimedsolutions.com>
2018-11-19 13:54:01 -0500

    Getting redux state to persist across form refreshes
    Task #218093

--- app/client/grocery_store/src/reducers/createStore.js
+++ app/client/grocery_store/src/reducers/createStore.js
@@ -1,13 +1,24 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import thunkMiddleware from 'redux-thunk';
 import reducer from './reducer';
 
 // eslint-disable-next-line no-underscore-dangle
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(
-  reducer,
+const persistConfig = {
+  storage,
+  key: 'root',
+  stateReconciler: autoMergeLevel2
+};
+
+const persistingReducer = persistReducer(persistConfig, reducer);
+
+export const store = createStore(
+  persistingReducer,
   composeEnhancers(applyMiddleware(thunkMiddleware))
 );
 
-export default store;
+export const persistor = persistStore(store);
