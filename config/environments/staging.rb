88005bb79915fbc7d69d57e18df2a8478b948ce6
Andrea Angott <andrea.angott@scimedsolutions.com>
2018-11-19 09:25:12 -0500

    Adding another config option

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -95,4 +95,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { host: 'howes-grocery.scimed-test.com' }
+  Rails.application.routes.default_url_options[:host] = 'howes-grocery.scimed-test.com'
 end
