4bae91bbcd06a71f6aea41470853fb87b408295f
Andrea Angott <andrea.angott@scimedsolutions.com>
2018-11-30 12:11:40 -0500

    Adding spec for budget manager
    Task #216930

---
+++ app/client/grocery_store/spec/utils/BudgetManager.test.js
@@ -0,0 +1,117 @@
+import BudgetManager from '../../src/utils/BudgetManager';
+
+describe('with a cart price of 0', () => {
+  const subject = new BudgetManager(0, 0, 0);
+
+  describe('subtotal', () => {
+    it('returns the expected string', () => {
+      expect(subject.subtotal()).toEqual('0.00');
+    });
+  });
+
+  describe('tax', () => {
+    it('returns the expected string', () => {
+      expect(subject.tax()).toEqual('0.00');
+    });
+  });
+
+  describe('total', () => {
+    it('returns the expected string', () => {
+      expect(subject.total()).toEqual('0.00');
+    });
+  });
+});
+
+describe('with a nonzero cart price', () => {
+  const subject = new BudgetManager(10, 0, 0);
+
+  describe('subtotal', () => {
+    it('returns the expected string', () => {
+      expect(subject.subtotal()).toEqual('10.00');
+    });
+  });
+
+  describe('tax', () => {
+    it('returns the expected string', () => {
+      expect(subject.tax()).toEqual('0.75');
+    });
+  });
+
+  describe('total', () => {
+    it('returns the expected string', () => {
+      expect(subject.total()).toEqual('10.75');
+    });
+  });
+});
+
+describe('when over max spend', () => {
+  // 9.31 will be 10.01 after tax
+  const subject = new BudgetManager(9.31, 10, 5);
+
+  describe('overMaxSpend', () => {
+    it('returns true', () => {
+      expect(subject.overMaxSpend()).toEqual(true);
+    });
+  });
+
+  describe('underMinSpend', () => {
+    it('returns false', () => {
+      expect(subject.underMinSpend()).toEqual(false);
+    });
+  });
+
+  describe('checkoutErrorMessage', () => {
+    it('returns the expected message', () => {
+      expect(subject.checkoutErrorMessage()).toEqual(
+        'In order to check out, you must spend less than $10.00.'
+      );
+    });
+  });
+});
+
+describe('when under min spend', () => {
+  // 4.64 will be 4.99 after tax
+  const subject = new BudgetManager(4.64, 10, 5);
+
+  describe('overMaxSpend', () => {
+    it('returns false', () => {
+      expect(subject.overMaxSpend()).toEqual(false);
+    });
+  });
+
+  describe('underMinSpend', () => {
+    it('returns true', () => {
+      expect(subject.underMinSpend()).toEqual(true);
+    });
+  });
+
+  describe('checkoutErrorMessage', () => {
+    it('returns the expected message', () => {
+      expect(subject.checkoutErrorMessage()).toEqual(
+        'In order to check out, you must spend more than $5.00.'
+      );
+    });
+  });
+});
+
+describe('when within budget constraints', () => {
+  const subject = new BudgetManager(6, 10, 5);
+
+  describe('overMaxSpend', () => {
+    it('returns false', () => {
+      expect(subject.overMaxSpend()).toEqual(false);
+    });
+  });
+
+  describe('underMinSpend', () => {
+    it('returns false', () => {
+      expect(subject.underMinSpend()).toEqual(false);
+    });
+  });
+
+  describe('checkoutErrorMessage', () => {
+    it('returns null', () => {
+      expect(subject.checkoutErrorMessage()).toEqual(null);
+    });
+  });
+});
