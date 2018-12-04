b1b1067dbb6d57d5ee364b9b76dd09387a0412a0
Andrea Angott <andrea.angott@scimedsolutions.com>
2018-12-04 12:06:53 -0500

    Adding some explanatory comments per code review
    Task #217816

--- app/services/checkout_products_exporter.rb
+++ app/services/checkout_products_exporter.rb
@@ -29,12 +29,16 @@ class CheckoutProductsExporter
       .group_by(&:session_identifier)
   end
 
+  # returns a hash where keys are column headers for the CSV, and values are the
+  # matching cell values for a row displaying a participant's product data.
   private def participant_row(session_identifier, results)
     { 'Participant' => session_identifier }
       .merge(all_product_fields(results))
       .merge(total_fields(results))
   end
 
+  # returns a hash is the aggregate of the `product_fields` hash for all
+  # products the participant had in their cart at checkout
   private def all_product_fields(results)
     # the format of this csv entails that every row has as many product columns
     # as the row for the participant with the most products. here we pad the
@@ -48,6 +52,16 @@ class CheckoutProductsExporter
     fields
   end
 
+  # returns a hash where keys are column headers and values are the matching
+  # cell values for a single product. columns include a 1-based index to
+  # differentiate among multiple products. looks like this:
+  #   {
+  #     'Item1_Quantity' => 2,
+  #     'Item1_Name' => 'Foo',
+  #     'Item1_Price' => 10.00,
+  #     'Item1_Calories_from_fat' => 100,
+  #     ...
+  #   }
   private def product_fields(result, index)
     product = result.try(:product)
     ordered_fields = %i[name price category] + nutrition_fields
@@ -57,6 +71,16 @@ class CheckoutProductsExporter
     { "Item#{index + 1}_Quantity" => result.try(:quantity) }.merge(product_data)
   end
 
+  # returns a hash where keys are column headers and values are the matching
+  # cell values capturing totals across all products in the cart.
+  # looks like this:
+  #   {
+  #     'TotalPrice_Pretax' => 30.25,
+  #     'TotalNumItems' => 5,
+  #     'Total_Calories_from_fat' => 500,
+  #     'Total_Calories' => 2500,
+  #     ...
+  #   }
   private def total_fields(results)
     products = results.flat_map do |result|
       Array.new(result.quantity, result.product)
