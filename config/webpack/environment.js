9f69fb7e6be890b493fac7d7cfeb8d2b538c4cc6
Andrea Angott <andrea.angott@scimedsolutions.com>
2019-06-03 10:00:35 -0400

    Removing obsolete config option

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,10 +4,7 @@ const customConfig = require('./custom');
 
 // resolve-url-loader must be used before sass-loader
 environment.loaders.get('sass').use.splice(-1, 0, {
-  loader: 'resolve-url-loader',
-  options: {
-    attempts: 1
-  }
+  loader: 'resolve-url-loader'
 });
 
 // Add an ProvidePlugin
