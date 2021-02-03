b1760eded1b95ad44cab763c279a35ca46e27339
Andrea Angott <andrea.angott@scimedsolutions.com>
2021-02-03 14:45:34 -0500

    Implementing file download for suggestions file
    Task #231839

--- app/controllers/product_downloads_controller.rb
+++ app/controllers/product_downloads_controller.rb
@@ -6,22 +6,26 @@
 # is detected and the file is delivered.
 class ProductDownloadsController < ApplicationController
   power :products, map: {
-    %i[custom_categories show] => :downloadable_products
+    %i[custom_categories suggestions show] => :downloadable_products
   }
 
   def custom_categories
+    redirect_to_download(CustomCategoryCsvManager)
+  end
+
+  def suggestions
+    redirect_to_download(SuggestionsCsvManager)
+  end
+
+  private def redirect_to_download(csv_generator_class)
     tempfile = Tempfile.new(filename)
-    tempfile.write(CustomCategoryCsvManager.generate_csv)
+    tempfile.write(csv_generator_class.generate_csv)
     url = product_download_path(filepath: tempfile.path)
     respond_to do |format|
       format.js do
         render js: "window.location.href = \"#{url}\";"
       end
     end
-  end
-
-  def suggestions
-
   end
 
   def show
