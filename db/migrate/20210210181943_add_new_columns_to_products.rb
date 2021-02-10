55a5ee31cdd4b806bb910a8be3c8b298eff05da5
Andrea Angott <andrea.angott@scimedsolutions.com>
2021-02-10 13:34:10 -0500

    Adding new product columns and updating product spreadsheet & exporter accordingly
    Task #231458

---
+++ db/migrate/20210210181943_add_new_columns_to_products.rb
@@ -0,0 +1,6 @@
+class AddNewColumnsToProducts < ActiveRecord::Migration[5.2]
+  def change
+    add_column :products, :serving_size_grams, :decimal, precision: 6, scale: 1
+    add_column :products, :caloric_density, :decimal, precision: 6, scale: 1
+  end
+end
