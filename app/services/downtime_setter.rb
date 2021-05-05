2ae4b7e2d7c8e6fa5e66d447b8a23a0ebb9439c0
Andrea Angott <andrea.angott@scimedsolutions.com>
2021-05-05 15:38:01 -0400

    Debugging turning database off and on
    Task #233268

--- app/services/downtime_setter.rb
+++ app/services/downtime_setter.rb
@@ -7,27 +7,41 @@
 # where the webserver (apache) has been configured to always show the file at
 # `public/system/maintenance.html` instead of the application if that file
 # exists.
+#
+# in order to give the server the right permissions to do these operations,
+# an IAM Role has been attached to the EC2 instance. that role has a policy
+# set that allows it to (only) start and stop its specific RDS instance.
 class DowntimeSetter
   def self.log
     @log ||= Logger.new(Rails.root.join('log/downtime.log'))
   end
 
-  def self.db_hostname
-    Rails.configuration.database_configuration[Rails.env]['host']
+  def self.db_identifier
+    host = Rails.configuration.database_configuration[Rails.env]['host']
+    host.split('.').first
   end
 
   def self.aws_client
     Aws::RDS::Client.new(region: 'us-east-1')
   end
 
+  def self.maintenance_page_location
+    Rails.root.join('public/system/maintenance.html')
+  end
+
   def self.turn_off_application
     log.info('Starting to put app into downtime.')
     FileUtils.cp(
       Rails.root.join('public/application_unavailable.html'),
-      Rails.root.join('public/system/maintenance.html'),
+      maintenance_page_location
     )
-    response = aws_client.stop_db_instance(db_instance_identifier: db_hostname)
-    log.info("Application put into downtime: #{response}")
+    response = aws_client.stop_db_instance(
+      db_instance_identifier: db_identifier
+    )
+    log.info(
+      'Application put into downtime: '\
+      "#{response.db_instance.db_instance_status}"
+    )
   rescue => e
     log.error("Error encountered: #{e.inspect}")
     log.error(e.backtrace.join("\n"))
@@ -35,9 +49,14 @@ class DowntimeSetter
 
   def self.turn_on_application
     log.info('Starting to bring app back from downtime.')
-    FileUtils.rm(Rails.root.join('public/system/maintenance.html'))
-    response = aws_client.stop_db_instance(db_instance_identifier: db_hostname)
-    log.info("Application brought back from downtime: #{response}")
+    FileUtils.rm(maintenance_page_location)
+    response = aws_client.start_db_instance(
+      db_instance_identifier: db_identifier
+    )
+    log.info(
+      'Application brought back from downtime: '\
+      "#{response.db_instance.db_instance_status}"
+    )
   rescue => e
     log.error("Error encountered: #{e.inspect}")
     log.error(e.backtrace.join("\n"))
