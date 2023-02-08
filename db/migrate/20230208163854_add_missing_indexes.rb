aa624148a9b1abca48579da37e0ab0cea5c96d4a
Andrea Angott <andrea.angott@scimedsolutions.com>
2023-02-08 14:53:19 -0500

    Changing method of deleting dependent records when parent record is deleted
    Task #241123

--- db/migrate/20230208163854_add_missing_indexes.rb
+++ db/migrate/20230208163854_add_missing_indexes.rb
@@ -2,10 +2,10 @@
 
 class AddMissingIndexes < ActiveRecord::Migration[5.2]
   def change
-    add_index :config_files, %i[id type] rescue ArgumentError
-    add_index :products, :category_id rescue ArgumentError
-    add_index :products, :subcategory_id rescue ArgumentError
-    add_index :subcategories, :category_id rescue ArgumentError
-    add_index :subcategory_exclusions, %i[condition_id subcategory_id] rescue ArgumentError
+    add_index :config_files, %i[id type]
+    add_index :products, :category_id
+    add_index :products, :subcategory_id
+    add_index :subcategories, :category_id
+    add_index :subcategory_exclusions, %i[condition_id subcategory_id]
   end
 end
