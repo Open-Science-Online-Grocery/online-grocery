efb04f26e97d867be77f741a52768923db9372e7
Andrea Angott <andrea.angott@scimedsolutions.com>
2023-02-08 12:07:26 -0500

    Debugging migration
    Task #241123

--- db/migrate/20230208163854_add_missing_indexes.rb
+++ db/migrate/20230208163854_add_missing_indexes.rb
@@ -2,10 +2,10 @@
 
 class AddMissingIndexes < ActiveRecord::Migration[5.2]
   def change
-    add_index :config_files, %i[id type], if_not_exists: true
-    add_index :products, :category_id, if_not_exists: true
-    add_index :products, :subcategory_id, if_not_exists: true
-    add_index :subcategories, :category_id, if_not_exists: true
-    add_index :subcategory_exclusions, %i[condition_id subcategory_id], if_not_exists: true
+    add_index :config_files, %i[id type] rescue ArgumentError
+    add_index :products, :category_id rescue ArgumentError
+    add_index :products, :subcategory_id rescue ArgumentError
+    add_index :subcategories, :category_id rescue ArgumentError
+    add_index :subcategory_exclusions, %i[condition_id subcategory_id] rescue ArgumentError
   end
 end
