6edb62950cdac858fa687a76847f4d99970dea98
Andrea Angott <andrea.angott@scimedsolutions.com>
2023-02-08 12:04:34 -0500

    Only creating indexes if they don't exist
    Task #241123

--- db/migrate/20230208163854_add_missing_indexes.rb
+++ db/migrate/20230208163854_add_missing_indexes.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class AddMissingIndexes < ActiveRecord::Migration[5.2]
   def change
-    add_index :config_files, [:id, :type]
-    add_index :products, :category_id
-    add_index :products, :subcategory_id
-    add_index :subcategories, :category_id
-    add_index :subcategory_exclusions, [:condition_id, :subcategory_id]
+    add_index :config_files, %i[id type], if_not_exists: true
+    add_index :products, :category_id, if_not_exists: true
+    add_index :products, :subcategory_id, if_not_exists: true
+    add_index :subcategories, :category_id, if_not_exists: true
+    add_index :subcategory_exclusions, %i[condition_id subcategory_id], if_not_exists: true
   end
 end
