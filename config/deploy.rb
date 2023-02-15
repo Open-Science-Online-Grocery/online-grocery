7a3e547d6833d72f2324d1817cd4d63aa125d55b
Thales Augusto <thalesmol57@gmail.com>
2023-02-15 15:14:37 -0300

    Updating staging config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock '~> 3.15.0'
+lock '~> 3.17.1'
 
 set :application, 'howes_grocery'
 set :repo_url, 'git@gitlab.com:scimedsolutions/HowesGrocery/howes_grocery_researcher_portal.git'
