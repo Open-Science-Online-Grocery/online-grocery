6fc31b2156851741830e9ff765088e01f1881a22
Ahmed Aziz <ahm.aziz12@gmail.com>
2024-02-15 07:17:14 +0500

    fixed issue of pricing and discount which were not working for auto populated cart

--- app/controllers/api/temp_carts_controller.rb
+++ app/controllers/api/temp_carts_controller.rb
@@ -32,8 +32,14 @@ module Api
         render json: [message: "No cart found with condition id = #{params[:condition_identifier]} and session_id = #{params[:session_id]}." ], status: :not_found
         return
       else
+        custom_attributes, custom_prices = load_custom_fields
         cart_attributes = @cart.attributes.merge(cart_items: @cart.cart_items.map do |ci|
-          ci.attributes.merge(product: ProductSerializer.new(ci.product, @condition).serialize)
+          ci.attributes.merge(product: ProductSerializer.new(ci.product, @condition,
+              {
+              custom_attribute_amount: custom_attributes[ci.product.id]
+                &.custom_attribute_amount,
+              custom_price_amount: custom_prices[ci.product.id]&.new_price
+            }).serialize)
         end )
         render json: cart_attributes
       end
@@ -60,6 +66,21 @@ module Api
         render json: { message: 'Please provide a valid Condition Identifier.' }, status: :unauthorized
         return
       end
+    end
+
+    def load_custom_fields
+      custom_attributes = custom_prices = []
+      if @condition.uses_custom_attributes?
+        custom_attributes = load_relational_data(:custom_product_attributes)
+      end
+      if @condition.uses_custom_prices?
+        custom_prices = load_relational_data(:custom_product_prices)
+      end
+      [custom_attributes, custom_prices]
+    end
+
+    def load_relational_data(relation_attr)
+      @condition.public_send(relation_attr).index_by(&:product_id) || []
     end
   end
 end
