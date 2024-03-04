9c0d9a81a206ab15dc55d8bab3aa4f2ae4ee3206
Andrea Angott <andrea.angott@scimedsolutions.com>
2024-03-04 10:38:48 -0500

    Bumping some spec-related gems and ensuring specs are run in random order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -41,6 +41,13 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = :random
+
   # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
   # have no way to turn it off -- the option exists only for backwards
   # compatibility in RSpec 3). It causes shared context metadata to be
